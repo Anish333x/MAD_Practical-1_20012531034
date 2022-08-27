de63d938a98a0b2caed4978c5e3db2ed65fc03a3
Anish333x <103912556+Anish333x@users.noreply.github.com>
2022-08-28 00:50:33 +0530

    Add files via upload

---
+++ p1_4.kt
@@ -0,0 +1,10 @@
+package com.example.p_1github
+
+fun main(){
+    print("Enter number : ")
+    var x:Int = readLine()!!.toInt()
+    if(x % 2 == 0)
+        println("Even")
+    else
+        println("Odd")
+}
---
+++ p1_9.kt
@@ -0,0 +1,21 @@
+package com.example.p_1github
+
+fun main() {
+    println("Enter numbers for array")
+    val a = ArrayList<Int>()
+    for (i in 0..5) {
+        print("a[$i] = ")
+        val temp: Int = readLine()!!.toInt()
+        a.add(temp)
+    }
+    println("Max number from array is ${maxNum(a)}")
+}
+fun maxNum(arr: ArrayList<Int>): Int {
+    var temp = arr[0]
+    for (i in 0..5) {
+        if (arr[i] > temp) {
+            temp = arr[i]
+        }
+    }
+    return temp
+}
